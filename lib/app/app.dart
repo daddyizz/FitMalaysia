02ebf93414661c1187b08283fe9f50d678c1cb7c
Daddy Izz <dady.izz85@gmail.com>
2026-07-17 16:48:48 +0800

    Setup Flutter localization for multilingual support

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../features/splash/splash_screen.dart';
 import 'app_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import '../l10n/app_localizations.dart';
 
 class FitMalaysiaApp extends StatelessWidget {
   const FitMalaysiaApp({super.key});
@@ -11,6 +13,19 @@ class FitMalaysiaApp extends StatelessWidget {
     return MaterialApp(
       title: 'FitMalaysia',
       debugShowCheckedModeBanner: false,
+
+      // 🌍 Localization
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
+      supportedLocales: const [
+        Locale('ms'),
+        Locale('en'),
+      ],
 
       // 🌞 Light Theme
       theme: AppTheme.lightTheme,
---
+++ lib/l10n/app_localizations_ms.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Malay (`ms`).
+class AppLocalizationsMs extends AppLocalizations {
+  AppLocalizationsMs([String locale = 'ms']) : super(locale);
+
+  @override
+  String get appName => 'FitMalaysia';
+
+  @override
+  String get home => 'Utama';
+
+  @override
+  String get workout => 'Senaman';
+
+  @override
+  String get nutrition => 'Pemakanan';
+
+  @override
+  String get settings => 'Tetapan';
+
+  @override
+  String get history => 'Sejarah';
+}
