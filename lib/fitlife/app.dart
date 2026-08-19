cceb686f7061ffc2fd64aee2f51d84cea4ffb352
Daddy Izz <dady.izz85@gmail.com>
2026-08-19 09:04:44 +0800

    Fine tune active switch styling

--- lib/fitlife/app.dart
+++ lib/fitlife/app.dart
@@ -6660,14 +6660,14 @@ class _AppSwitch extends StatelessWidget {
     final scheme = Theme.of(context).colorScheme;
     return Transform.scale(
       scaleX: .82,
-      scaleY: .72,
+      scaleY: .70,
       child: DecoratedBox(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(999),
           boxShadow: !isDark && value
               ? const [
                   BoxShadow(
-                    color: Color(0x73152015),
+                    color: Color(0x12152015),
                     blurRadius: 2,
                     offset: Offset(0, 1),
                   ),
